f2da4f1a85d8160f7b05cc25a05b9aed34644ecc
Samson Nwokike <nwokikesamson@gmail.com>
2023-05-20 23:28:55 +0100

    added mongodb to docker compose

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -51,20 +51,29 @@ func main(){
 	}
 
 	// start web server
-	go app.serve()
-}
-
-func (app *Config) serve(){
+	// go app.serve()
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%s", PORT),
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic()
 	}
 }
+
+// func (app *Config) serve(){
+// 	srv := &http.Server{
+// 		Addr: fmt.Sprintf(":%s", PORT),
+// 		Handler: app.routes(),
+// 	}
+
+// 	err := srv.ListenAndServe()
+// 	if err != nil {
+// 		log.Panic()
+// 	}
+// }
 
 func connectToMongo() (*mongo.Client, error){
 	// create connection options
