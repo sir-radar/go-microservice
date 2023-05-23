c78aa2951db79a7dd1f71730a75eb4779b64bac1
Samson Nwokike <nwokikesamson@gmail.com>
2023-05-23 21:57:13 +0100

    mailer service complete setup

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type Config struct {
 
 }
@@ -7,5 +13,17 @@ type Config struct {
 const PORT = "80"
 
 func main(){
+	app := Config{}
 
+	log.Println("Starting mail service on port", PORT)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", PORT),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
